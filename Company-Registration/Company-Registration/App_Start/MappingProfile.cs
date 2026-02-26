3de9fc22eeae38562fd4e5f7531a683034d2fe6e
Gem123578 <maythagyan.mtg.gl@gmail.com>
2026-02-27 00:07:56 +0630

    api

---
+++ Company-Registration/Company-Registration/App_Start/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Company_Registration.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Mapper.CreateMap<Models.CompanyApplicant, ViewModels.CompanyApplicantViewModel>();
+        }
+    }
+}
