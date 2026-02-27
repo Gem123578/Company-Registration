9f94122612ea956c6f2b176487d4a15ef675633c
Gem123578 <maythagyan.mtg.gl@gmail.com>
2026-02-27 07:38:16 +0630

    api

---
+++ Company-Registration/Company-Registration/DTO/CompanyApplicantDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Company_Registration.DTO
+{
+    public class CompanyApplicantDTO
+    {
+
+    }
+}
---
+++ Company-Registration/Company-Registration/Models/CompanyApplicant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Company_Registration.Models
+{
+    public class CompanyApplicant
+    {
+        public long Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Nationality { get; set; }
+        public string IdentityNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
